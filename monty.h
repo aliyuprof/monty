0213fb085daeb83a97ddadf175b2195f94059088
aliyuprof <aliyuddeen95@gmail.com>
2024-03-28 17:15:01 -0700

    Proram rectified

--- monty.h
+++ monty.h
@@ -12,6 +12,7 @@
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
  * @next: points to the next element of the stack (or queue)
+ * by Ali USMAN Mohammed
  *
  * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO Holberton project
